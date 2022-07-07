adc006da0b76e5448bfdf24f7ad5c0a1a0312479
SDVISTANBUL <leylasena15@gmail.com>
2022-07-07 21:52:55 +0100

    Next-788 new

--- src/test/java/com/nextBase/step_definitions/NegativeNonCharacter_StepDefinitions.java
+++ src/test/java/com/nextBase/step_definitions/NegativeNonCharacter_StepDefinitions.java
@@ -1,5 +1,6 @@
 package com.nextBase.step_definitions;
 
+import com.nextBase.pages.BasePage;
 import com.nextBase.pages.NextBasePage;
 import com.nextBase.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -11,6 +12,22 @@ import org.openqa.selenium.Keys;
 public class NegativeNonCharacter_StepDefinitions {
 
     NextBasePage nextBasePage = new NextBasePage();
+
+    @Given("{string} is on homepage")
+    public void is_on_homepage(String string) {
+        switch (string.toLowerCase()){
+            case "hr":
+                BasePage.loginAsHR();
+                break;
+            case "helpdesk":
+                BasePage.loginAsHelpdesk();
+                break;
+            case "marketing":
+                BasePage.loginAsMarketing();
+                break;
+        }
+    }
+
 
     @Given("clicks search button with non-releated {string}")
     public void clicks_search_button_with_non_releated(String string) {
