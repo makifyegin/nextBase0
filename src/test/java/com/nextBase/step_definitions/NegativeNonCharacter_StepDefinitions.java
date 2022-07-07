a62a70f614025c53ef5f5ef4aa8ebfd9e8416396
SDVISTANBUL <leylasena15@gmail.com>
2022-07-07 21:52:55 +0100

    Next-788 new

--- src/test/java/com/nextBase/step_definitions/NegativeNonCharacter_StepDefinitions.java
+++ src/test/java/com/nextBase/step_definitions/NegativeNonCharacter_StepDefinitions.java
@@ -3,14 +3,11 @@ package com.nextBase.step_definitions;
 import com.nextBase.pages.BasePage;
 import com.nextBase.pages.NextBasePage;
 import com.nextBase.utilities.Driver;
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-
-import java.util.Locale;
 
 public class NegativeNonCharacter_StepDefinitions {
 
@@ -30,11 +27,8 @@ public class NegativeNonCharacter_StepDefinitions {
                 break;
         }
     }
-//    @Given("User is on homepage")
-//    public void is_on_homepage(DataTable dataTable) {
-//        dataTable.asLists().get(0).toString().toUpperCase().contains("HR");
-//        dataTable.asMap().get("USER").contains("HR");// dataTable should start with USER - And, not sure if it's gonna work
-//    }
+
+
     @Given("clicks search button with non-releated {string}")
     public void clicks_search_button_with_non_releated(String string) {
         nextBasePage.searchBtn.sendKeys(string + Keys.ENTER);
@@ -47,3 +41,4 @@ public class NegativeNonCharacter_StepDefinitions {
 
 
 }
+
