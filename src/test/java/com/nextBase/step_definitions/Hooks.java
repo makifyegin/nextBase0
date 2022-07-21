e1561e8f2759b1084dbd45c7e5624ce49145c6d9
SDVISTANBUL <leylasena15@gmail.com>
2022-07-21 19:36:29 +0100

    Next-1018 new

--- src/test/java/com/nextBase/step_definitions/Hooks.java
+++ src/test/java/com/nextBase/step_definitions/Hooks.java
@@ -48,9 +48,8 @@ public class Hooks {
         }
 
 
-        BrowserUtils.sleep(2);
-        //Driver.closeDriver();
-        BrowserUtils.sleep(2);
+        BrowserUtils.sleep(5);
+        Driver.closeDriver();
 
         //System.out.println("====Closing browser using cucumber @After");
         //System.out.println("====Scenario ended/ Take screenshot if failed!");
