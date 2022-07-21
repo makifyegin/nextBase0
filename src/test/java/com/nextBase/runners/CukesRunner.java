5d4f31f192b1e82b4082fbb6e6b3598123997b48
SDVISTANBUL <leylasena15@gmail.com>
2022-07-21 07:25:42 +0100

    Next-1018 new

--- src/test/java/com/nextBase/runners/CukesRunner.java
+++ src/test/java/com/nextBase/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/nextBase/step_definitions",
         dryRun = false,
-        tags = "@uploadApp",
+        tags = "@NEXT-1043",
         publish = false
 
 
