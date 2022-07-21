1b3902def66b040039bf43a8687628c2849b2a12
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-21 16:41:42 +0100

    I implemented all the feature files and step definitions except last test Cases

--- src/test/java/com/nextBase/step_definitions/Next_1012_StepDefinitions.java
+++ src/test/java/com/nextBase/step_definitions/Next_1012_StepDefinitions.java
@@ -1,258 +1,246 @@
 package com.nextBase.step_definitions;
 
-import com.nextBase.pages.CalendarPage;
-import com.nextBase.utilities.BrowserUtils;
-import com.nextBase.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Next_1012_StepDefinitions {
-    CalendarPage calendarPage = new CalendarPage();
 
     @Given("Click the This event is important")
     public void click_the_this_event_is_important() {
-
-        calendarPage.
-                thisEventIsImportant.click();
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Fill out the {string}")
     public void fill_out_the(String string) {
-        string = "MY_FIRST_DESCRIPTION_OF_BIRTHDAY";
-        calendarPage.eventNameInput.clear();
-        calendarPage.eventNameInput.sendKeys(string);
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Specifying {string} dates in the spesific {string}")
     public void specifying_dates_in_the_spesific(String string, String string2) {
-        string = "25/08/2022";
-        string2 = "30/08/2022";
-        calendarPage.eventDateAndTime.clear();
-        calendarPage.eventDateAndTime.sendKeys(string);
-        calendarPage.eventEndDateAndTime.clear();
-        calendarPage.eventEndDateAndTime.sendKeys(string2);
-
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Click the repeating event with description")
     public void click_the_repeating_event_with_description() {
-       Select select = new Select(calendarPage.repeatSelector);
-       select.selectByIndex(1);
-
-
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Add {string} time zones")
     public void add_time_zones(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
-
-
     @Given("Add certain {string}")
     public void add_certain(String string) {
-        Select select = new Select(calendarPage.loCationInput);
-        select.selectByIndex(3);
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Add {string}")
     public void add(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Click More")
     public void click_more() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Add colour of {string}")
     public void add_colour_of(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("Choose availability {string}")
     public void choose_availability(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Given("user click save button")
     public void user_click_save_button() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Given("Click the Event")
     public void click_the_event() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @When("Click the Open")
     public void click_the_open() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("Verify that Event name is {string}")
     public void verify_that_event_name_is(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("Verify that start day is {string} end date {string}")
     public void verify_that_start_day_is_end_date(String string, String string2) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("Verify that {string} event is not selected")
     public void verify_that_event_is_not_selected(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("Verify that colour is \\{Pink}")
     public void verify_that_colour_is() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
 
 
     @Given("Click the Edit")
     public void click_the_edit() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @When("Click other color")
     public void click_other_color() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
+
+
 
 
     @When("Click Private Event")
     public void click_private_event() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Then("Verify that Private event is selected.")
     public void verify_that_private_event_is_selected() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Given("Click Logout")
     public void click_logout() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @When("List the Event")
     public void list_the_event() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("Verify that Marketing User can not see the private event")
     public void verify_that_marketing_user_can_not_see_the_private_event() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
+
+
 
 
     @When("Choose available {string}")
     public void choose_available(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
 
 
     @Then("Verify that Available is {string}")
     public void verify_that_available_is(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
 
 
     @Then("verify that event {string} changed")
     public void verify_that_event_changed(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
 
     @When("Click the More")
     public void click_the_more() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @When("Choose the {string} repeat")
     public void choose_the_repeat(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("verify that this event is happen daily")
     public void verify_that_this_event_is_happen_daily() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
 
 
     @When("Click the delete attendee")
     public void click_the_delete_attendee() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Then("Clock the Open")
     public void clock_the_open() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("Verify that event attend's was updated")
     public void verify_that_event_attend_s_was_updated() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
 
     @When("Click the add {string} and {string} attendee")
     public void click_the_add_and_attendee(String string, String string2) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
 
     @Then("Verify that event attendees was updated")
     public void verify_that_event_attendees_was_updated() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @When("Click the Invitation {string}")
     public void click_the_invitation(String string) {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @When("Click the Search")
     public void click_the_search() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Then("Verify that User can displays these events")
     public void verify_that_user_can_displays_these_events() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @When("Click the I am an Organiser")
     public void click_the_i_am_an_organiser() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
+
 
 
     @And("Verify that colour is {string}")
     public void verifyThatColourIs(String arg0) {
-
     }
-
-
-    @And("Add [attendees]")
-    public void addAttendees() {
-        calendarPage.attendees.click();
-        calendarPage.attendeesHelpDesk.click();
-    }
-
 }
