81c122d3442340853caad4e18603ec6c2ff7aba4
ramazanyildirim1 <97792743+ramazanyildirim1@users.noreply.github.com>
2022-07-22 14:31:49 +0100

    last commit

--- src/test/java/com/nextBase/step_definitions/Hr_Task_Negative_StepDefinitions.java
+++ src/test/java/com/nextBase/step_definitions/Hr_Task_Negative_StepDefinitions.java
@@ -30,11 +30,9 @@ public class Hr_Task_Negative_StepDefinitions {
         Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='side-panel-iframe']")));
         //System.out.println("Driver.getDriver().getCurrentUrl() = " + Driver.getDriver().getCurrentUrl());
         String expectedContainsUrl = "task/edit";
-
         Assert.assertTrue(expectedContainsUrl,
                 Driver.getDriver().getCurrentUrl().contains("task/edit"));
     }
-
     @And("User  clicks ADD TASK button")
     public void userClicksADDTASKButton() {
         hrTaskPage.addTaskEnter.click();
@@ -44,9 +42,7 @@ public class Hr_Task_Negative_StepDefinitions {
     public void userDisplays(String arg0) {
         String expected= "Task name is not specified.";
       Assert.assertTrue(hrTaskPage.newTaskErrorMessage.isDisplayed());
-
     }
-
     @And("user  clicks on header  and types {string}.")
     public void userClicksOnHeaderAndTypes(String arg0) {
         hrTaskPage.thingsToDo.sendKeys( arg0);
@@ -58,35 +54,24 @@ public class Hr_Task_Negative_StepDefinitions {
         hrTaskPage.textArea.click();
         hrTaskPage.textArea.sendKeys(arg0);
     }
-
     @And("User  clicks Deadline dropdown menu")
     public void userClicksDeadlineDropdownMenu() {
         //Driver.getDriver().switchTo().parentFrame();
         hrTaskPage.deadline.click();
-    }
-
-    @And("user clicks left arrow button on calender.")
+    }@And("user clicks left arrow button on calender.")
     public void userClicksLeftArrowButtonOnCalender() throws InterruptedException {
         Thread.sleep(4);
         hrTaskPage.calendarLeftButton.click();
     }
-
     @And("User  select {string} date")
     public void userSelectDate(String arg0) {
         hrTaskPage.date10ThJun.click();
         hrTaskPage.selectButton.click();
     }
-
     @Then("Verify that task hasn't been created recent time")
     public void userCanNotCreateTaskRecentTime() throws InterruptedException {
         Driver.getDriver().switchTo().defaultContent();
         hrTaskPage.overDueButton.click();
         //Assert.assertTrue(hrTaskPage.overDueButton.isDisplayed());
     }
-
-
-
-
-
-
 }
